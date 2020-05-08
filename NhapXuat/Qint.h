0b9edd1b7382f4ee0fcd27ed996df39f0ffa9189
ptbminh1108 <43947847+ptbminh1108@users.noreply.github.com>
2020-05-08 21:09:11 +0700

    Add files via upload
    Qint - g,h,i - completed

--- NhapXuat/Qint.h
+++ NhapXuat/Qint.h
@@ -14,7 +14,8 @@ private:
 public:
 	QInt();
 	QInt(const QInt& x);
-	QInt(const string &Bin); // Chuyển chuỗi nhị phân thành QInt
+
+	QInt(const string& Bin); // Chuyển chuỗi nhị phân thành QInt
 	~QInt();
 
 	// Hàm kiểm tra mở rộng
@@ -33,13 +34,47 @@ public:
 	QInt& rol();
 	QInt& ror();
 
+
+	//18120210
+	QInt bu1();
+	QInt bu2();
+	// operator +,-,*,/ - 18120210
+	friend QInt operator+(QInt a, QInt b);
+	friend QInt operator-(QInt a, QInt b);
+	friend QInt operator*(QInt a, QInt b);
+	friend QInt operator/(QInt a, QInt b);
+
+	// operator: AND “&”, OR “|”, XOR “^”, NOT “~” - 18120210
+	friend QInt operator&(QInt a, QInt b);
+	friend QInt operator|(QInt a, QInt b);
+	friend QInt operator^(QInt a, QInt b);
+	QInt operator~();
+
+	// operator “<”, “>”, “ == ”, “ <= ”, “ >= ” - 18120210
+	friend bool operator>(QInt a, QInt b);
+	friend bool operator<(QInt a, QInt b);
+	friend bool operator==(QInt a, QInt b);
+	friend bool operator>=(QInt a, QInt b);
+	friend bool operator<=(QInt a, QInt b);
+	
+
+
+
+
 	string QInttoBin();		// Chuyển QInt thành chuỗi nhị phân
 protected:
+	int getBitPositionI(int i); // Lấy bit vị trí i - 18120210
+	void setBitPositionIto1(int i); // set bit vị trí i = 1 - 18120210
+	void setBitPositionIto0(int i); // set bit vị tri i = 0 - 18120210
+
 	string StrDivTwo(string src);	// Chuỗi số chia cho 2
 	string StrDectoBin(string src);	// Chuyển chuỗi Dec thành chuỗi nhị phân
 	string SumString(string a, string b);	// tổng hai chuỗi số
 	string Double(string src);	// 2*src
 	string QInttoStrDec();	// Chuyển QInt thành chuỗi thập phân
-	
+
+
+	//my code
+
 };
 
